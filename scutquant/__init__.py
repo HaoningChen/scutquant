7d15649c6a3f8705f2fc12cd444da678d813b4e4
Haoning Chen <101194077+HaoningChen@users.noreply.github.com>
2023-01-06 21:27:13 +0800

    Add files via upload

---
+++ scutquant/__init__.py
@@ -0,0 +1,9 @@
+from .scutquant import *
+from .alpha import *
+from .strategy import *
+from .executor import *
+from .account import *
+from .report import *
+from .signal_generator import *
+
+__version__ = "0.0.1"
