69e230bad2a28184d72a7ccd0d4afb1afdef43fa
Haoning Chen <101194077+HaoningChen@users.noreply.github.com>
2023-03-31 17:56:18 +0800

    Delete __init__.py

--- scutquant/__init__.py
+++
@@ -1,9 +0,0 @@
-from .scutquant import *
-from .alpha import *
-from .strategy import *
-from .executor import *
-from .account import *
-from .report import *
-from .signal_generator import *
-
-__version__ = "0.0.1"
