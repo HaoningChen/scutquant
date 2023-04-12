5379ff5a4e5bf012387164341f1924195322f4e0
Haoning Chen <101194077+HaoningChen@users.noreply.github.com>
2023-04-12 18:39:04 +0800

    完善了make_pca函数

--- scutquant/scutquant.py
+++ scutquant/scutquant.py
@@ -162,7 +162,39 @@ def make_pca(X):
     X_pca = pca.fit_transform(X)
     component_names = [f"PC{i + 1}" for i in range(X_pca.shape[1])]
     X_pca = pd.DataFrame(X_pca, columns=component_names, index=index)
-    return X_pca
+    loadings = pd.DataFrame(
+        pca.components_.T,  # transpose the matrix of loadings
+        columns=component_names,  # so the columns are the principal components
+        index=X.columns,  # and the rows are the original features
+    )
+    result = {
+        "pca": pca,
+        "loadings": loadings,
+        "X_pca": X_pca
+    }
+    return result
+
+
+def plot_pca_variance(pca):
+    # Create figure
+    fig, axs = plt.subplots(1, 2)
+    n = pca.n_components_
+    grid = np.arange(1, n + 1)
+    # Explained variance
+    evr = pca.explained_variance_ratio_
+    axs[0].bar(grid, evr)
+    axs[0].set(
+        xlabel="Component", title="% Explained Variance", ylim=(0.0, 1.0)
+    )
+    # Cumulative Variance
+    cv = np.cumsum(evr)
+    axs[1].plot(np.r_[0, grid], np.r_[0, cv], "o-")
+    axs[1].set(
+        xlabel="Component", title="% Cumulative Variance", ylim=(0.0, 1.0)
+    )
+    # Set up figure
+    fig.set(figwidth=8, dpi=100)
+    return axs
 
 
 def symmetric(X):  # 对称正交
